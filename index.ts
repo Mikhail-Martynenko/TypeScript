b5030e2b1a7365cde632474ad876bb48410c05db
Mikhail Martynenko <misha.martynenko8@yandex.ru>
2023-05-04 18:07:20 +0300

    107965 feat: added the block TypeScript

---
+++ index.ts
@@ -0,0 +1,76 @@
+// TODO Enum
+enum EActionMap {
+    ADD = 'add',
+    DELETE = 'delete',
+    UPDATE = 'update',
+}
+
+const actionMap: EActionMap[] = [
+    EActionMap.ADD, EActionMap.DELETE, EActionMap.UPDATE
+];
+
+// TODO Interface
+
+interface IResult {
+    0: string;
+    1: number;
+}
+
+interface IFunc {
+    (id: number, isActive: boolean, callback: () => void): IResult[];
+}
+
+const func: IFunc = (id, isActive, callback) => {
+    return [
+        ['name', 123]
+    ]
+}
+func(1, true, () => {
+});
+
+// TODO Interface
+interface IItem {
+    id: number;
+    name: string;
+    items: [] | null;
+}
+
+interface IA {
+    id: number;
+    name: string;
+    items: IItem[]
+}
+
+const a: IA = {
+    id: 123,
+    name: 'Eric',
+    items: [
+        {
+            id: 55,
+            name: 'Masha',
+            items: []
+        },
+        {
+            id: 34,
+            name: 'Max',
+            items: null
+        }
+    ]
+};
+
+interface IObjectN {
+    [key: string]: (number) => string;
+}
+
+// Ключ может оказаться любым. Нужно создать интерфейс, описывающий объект n (типизировать ключи и значения)
+const n: IObjectN = {
+    smth: function (number) {
+        return 'test';
+    },
+    smth2: function (number) {
+        return 'test';
+    },
+    smth3: function (number) {
+        return 'test';
+    }
+};
---
+++ task2TS.ts
@@ -0,0 +1,30 @@
+//1. Написать тип для функции, которая принимает один аргумент - число или строку. Если пришло число - возвращает {d: 1}, если пришла строка {z: 2}
+
+function f<T extends string | number>(arg: T): T extends number ? { d: 1 } : { z: 2 } {
+    return typeof arg === 'number' ? {d: 1} : {z: 2} as any;
+}
+
+f(1).d
+f("t").z
+
+//2. Реализовать класс SomeClass:
+
+type TSomeClassArgs = Record<string, unknown>;
+
+class SomeClass<T extends TSomeClassArgs> {
+    run<K extends keyof T>(methodName: K, args: T[K] | null): void {
+        console.log(`Called ${String(methodName)} with argument ${args}`);
+    }
+}
+
+const a = new SomeClass<
+    {
+        runMethodOne: number;
+        runMethodTwo: void;
+    }
+>();
+
+a.run("runMethodOne", 1);
+a.run("runMethodTwo", null);
+
+//a.run("runMethodsdjhfbs", null); // Type error
